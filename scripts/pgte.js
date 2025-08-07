dbe38c6ae26c8cd42d358bd928985e8b3f2d1931
Adnominatio <glick.casey@gmail.com>
2025-08-07 09:22:42 -0700

    added times and subtract to js

--- scripts/pgte.js
+++ scripts/pgte.js
@@ -1,3 +1,16 @@
+Handlebars.registerHelper("times", function(n, block) {
+  let accum = "";
+  for (let i = 0; i < n; ++i) {
+    accum += block.fn(i);
+  }
+  return accum;
+});
+
+Handlebars.registerHelper("subtract", function(a, b) {
+  return a - b;
+});
+
+
 class PGTEActorSheet extends ActorSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
